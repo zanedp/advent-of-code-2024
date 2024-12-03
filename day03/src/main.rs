676d387c1da86203b0fa3c4c716873e3d320c679
Zane D. Purvis <295055+zanedp@users.noreply.github.com>
2024-12-03 11:44:39 -0500

    Add day03, pt2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,18 +3,31 @@ use regex::Regex;
 const EXAMPLE_INPUT: &str =
     r#"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"#;
 fn main() {
-    let input = EXAMPLE_INPUT;
+    // let input = EXAMPLE_INPUT;
     let input = include_str!("input.txt");
 
-    let re = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").unwrap();
-    let mul_sum = re
+    let mul_sum_pt1 = get_pairs(input).iter().map(|(a, b)| a * b).sum::<u64>();
+    dbg!(mul_sum_pt1);
+
+    // let input = r#"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"#;
+    let enabled_at_start = input.split("do()");
+    let disables_removed = enabled_at_start.map(|x| x.split("don't()").next().unwrap());
+    let recombined = disables_removed.collect::<String>();
+    let mul_sum_pt2 = get_pairs(&recombined)
+        .iter()
+        .map(|(a, b)| a * b)
+        .sum::<u64>();
+    dbg!(mul_sum_pt2);
+}
+
+fn get_pairs(input: &str) -> Vec<(u64, u64)> {
+    Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)")
+        .unwrap()
         .captures_iter(input)
         .map(|captures| {
             let a = captures.name("a").unwrap().as_str().parse::<u64>().unwrap();
             let b = captures.name("b").unwrap().as_str().parse::<u64>().unwrap();
             (a, b)
         })
-        .map(|(a, b)| a * b)
-        .sum::<u64>();
-    dbg!(mul_sum);
+        .collect()
 }
