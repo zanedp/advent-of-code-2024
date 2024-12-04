b6280b3527c9eeac175eed3ee7a63186f2c251e0
Zane D. Purvis <295055+zanedp@users.noreply.github.com>
2024-12-04 13:05:08 -0500

    day03: Improve performance

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::sync::LazyLock;
 
 // const EXAMPLE_INPUT: &str =
 //     r#"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"#;
@@ -9,28 +10,25 @@ fn main() {
     let input = EXAMPLE_INPUT;
     // let input = include_str!("input.txt");
 
-    let mul_sum_pt1 = get_pairs(input).iter().fold(0, |acc, (x, y)| x * y + acc);
+    let mul_sum_pt1 = get_pairs(input).fold(0, |acc, (x, y)| x * y + acc);
     dbg!(mul_sum_pt1);
 
     let start_time = std::time::Instant::now();
     let enabled_at_start = input.split("do()");
     let disables_removed = enabled_at_start.map(|x| x.split("don't()").next().unwrap());
     let recombined = disables_removed.collect::<String>();
-    let mul_sum_pt2 = get_pairs(&recombined)
-        .iter()
-        .fold(0, |acc, (x, y)| x * y + acc);
+    let mul_sum_pt2 = get_pairs(&recombined).fold(0, |acc, (x, y)| x * y + acc);
     dbg!(start_time.elapsed());
     dbg!(mul_sum_pt2);
 }
 
-fn get_pairs(input: &str) -> Vec<(u64, u64)> {
-    Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)")
-        .unwrap()
-        .captures_iter(input)
-        .map(|captures| {
-            let a = captures.name("a").unwrap().as_str().parse::<u64>().unwrap();
-            let b = captures.name("b").unwrap().as_str().parse::<u64>().unwrap();
-            (a, b)
-        })
-        .collect()
+fn get_pairs(input: &str) -> impl Iterator<Item = (u64, u64)> + use<'_> {
+    static RE: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").unwrap());
+
+    RE.captures_iter(input).map(|captures| {
+        let a = captures.name("a").unwrap().as_str().parse::<u64>().unwrap();
+        let b = captures.name("b").unwrap().as_str().parse::<u64>().unwrap();
+        (a, b)
+    })
 }
